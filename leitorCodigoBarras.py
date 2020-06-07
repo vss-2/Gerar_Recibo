9a3983c01804916b12a4bc7476ed7e3056741f23
vss-2 <32624576+vss-2@users.noreply.github.com>
2020-06-06 23:03:38 -0300

    Create leitorCodigoBarras.py

---
+++ leitorCodigoBarras.py
@@ -0,0 +1,14 @@
+import cv2
+import pyperclip
+from pyzbar.pyzbar import decode
+
+if __name__ == "__main__":
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 640)
+        cap.set(4, 480)
+        
+        while True:
+                for cdg_barra in decode(img):
+                        if(cdg_barra.type == 'code128'):
+                                pyperclip.paste(str(cdg_barra.data.decode('utf-8')))
+                                exit()
